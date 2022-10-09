2f83b66468909ba152dd6abf460eba36b4549236
Rishabh Kumar <rishabh_kumar@epam.com>
2022-10-09 14:28:46 +0530

    'modified_file'

--- read_excel_file.py
+++ read_excel_file.py
@@ -5,8 +5,9 @@ sheet_obj = dt.active
 max_column = sheet_obj.max_column
 max_row = sheet_obj.max_row
 
+print("Values of excel sheet are below")
 for i in range(1,max_column+1):
     for j in range(1,max_row+1):
         cell_value = sheet_obj.cell(row=j, column=i).value
-        #print('\n')
         print(cell_value, end=' ')
+
